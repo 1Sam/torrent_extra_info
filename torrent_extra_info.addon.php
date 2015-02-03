a7959d4d7099ae606eea7e923bd0d4117b68bd93
1Sam Online <csh@korea.com>
2015-02-03 12:06:09 +0900

    Update torrent_extra_info.addon.php

--- torrent_extra_info.addon.php
+++ torrent_extra_info.addon.php
@@ -9,27 +9,19 @@ if(!defined('__XE__')) exit();
  * @brief torrent_extra_info addon
  *
  * 첨부된 토렌트 파일의 정보를 자동으로 출력해 주는 애드온입니다.
+ * Bootstrap 속성을 사용하여 출력합니다.
  **/
 
 // return unless before_display_content
-if($called_position != "before_display_content" || Context::get('act') == 'dispPageAdminContentModify' || Context::getResponseMethod() != 'HTML' || isCrawler())
-{
-	return;
-}
+if($called_position != "before_display_content" || Context::get('act') == 'dispPageAdminContentModify' || Context::getResponseMethod() != 'HTML' || isCrawler()) return;
 
 Context::addCSSFile('./addons/torrent_extra_info/css/addon.css');
 
 require_once(_XE_PATH_ . 'addons/torrent_extra_info/torrent_extra_info.lib.php');
-
 require_once(_XE_PATH_ . 'addons/torrent_extra_info/torrent_extra_info.api.php');
 
-if($addon_info->torrent_css == "Y") {
-	//Bootstrap 태그로 작성
-	$torrentExtraInfoTrans = 'torrentExtraInfoTrans';
-} else {
-	// 대충 작성
-	$torrentExtraInfoTrans = 'torrentExtraInfoTrans2';
-}
+// 콜백으로 사용될 함수명 지정
+$torrentExtraInfoTrans = $addon_info->torrent_css == "Y" ? 'torrentExtraInfoTrans' : 'torrentExtraInfoTrans2';
 
 if($addon_info->torrent_tag == 'all') {
 	$temp_output = preg_replace_callback('!<(div|span|a)([^\>]*)>([^\>]*)('.$addon_info->torrent_extention.')(.*?)\<\/(div|span|a)\>!is', $torrentExtraInfoTrans, $output);
@@ -41,6 +33,8 @@ if($addon_info->torrent_tag == 'all') {
 	//$temp_output = preg_replace_callback("/<".$addon_info->torrent_tag."[^>]*sid=*([^'\"]+)['\"]*[^>]*>.+?".$addon_info->torrent_extention.".+?<\/".$addon_info->torrent_tag.">?/", $torrentExtraInfoTrans, $output);
 
 	// 직접 sid 검출 source_filename 문자열에 "sid"가 있을 경우를 대비해 sid 앞에 오는 &(&amp;) 추가
+
+	// 정규식끝에 i 추가해서 torrent or TORRENT 대소문자 구별 안하게 함
 	$temp_output = preg_replace_callback("/<".$addon_info->torrent_tag."[^>]*&amp;sid=*([^'\"]+)&amp;['\"]*[^>]*>.*?".$addon_info->torrent_extention.".+?<\/".$addon_info->torrent_tag.">?/i", $torrentExtraInfoTrans, $output);
 	
 	// 직접 sid 검출 source_filename 문자열에 "sid"가 있을 경우를 대비해 sid 앞에 오는 &(&amp;) 추가
